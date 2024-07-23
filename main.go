fcd68f4d29e88e3499c7cafe5335be2bab338f17
Mushr00mZhang <392388987@qq.com>
2024-07-23 23:44:41 +0800

    添加时区配置

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"os"
 	"server/websocket"
+	"strings"
+	"time"
+	_ "time/tzdata"
 )
 
 func main() {
+	// 设置timezone，默认Asia/Shanghai
+	tz := os.Getenv("TZ")
+	if strings.TrimSpace(tz) == "" {
+		tz = "Asia/Shanghai"
+	}
+	time.LoadLocation(tz)
 	// server := NewServer("", 8888)
 	// go server.Start()
 	ws := websocket.NewServer("", 8889)
