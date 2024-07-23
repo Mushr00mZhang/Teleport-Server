1385139c5767263bb7f81ccac402d9ceea420a8c
Mushr00mZhang <392388987@qq.com>
2024-07-23 20:52:29 +0800

    取消跨域验证

--- websocket/client.go
+++ websocket/client.go
@@ -31,6 +31,7 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  4096,
 	WriteBufferSize: 4096,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 type Client struct {
